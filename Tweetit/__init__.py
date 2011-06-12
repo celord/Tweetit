740ea761d94517db4144504b834cf14cd834701a
Cesar Garcia <celord@gmail.com>
2011-06-12 13:14:15 -0600

    Fixing the author's name Documenting the project

--- Tweetit/__init__.py
+++ Tweetit/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import rb
 class Tweetit(rb.Plugin):
+"""
+Tweetit class derived from rb.Plugin at te moment it only activates and 
+deactivates the plugin that does not do nothing 
+"""
     def activate(self, shell):
         print "Activate!"
     def deactivate(self, shell):
